2bf84bb140bc9bc9259818178433ac9127b4ec3c
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-05 17:13:00 -0300

    form with mentor field hidden

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit::Authorization
 
   after_action :verify_authorized, except: %i[root contact resources], unless: :skip_pundit?
   after_action :verify_policy_scoped, except: %i[root contact resources], unless: :skip_pundit?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country, :state, :city, :professional_field, :academic_degree, :programming_language, :mentor_current_employer, :mentor_transition_date, :github, :linkedin, :personal_site, :other_info, :mentor])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :country, :state, :city, :professional_field, :academic_degree, :programming_language, :mentor_current_employer, :mentor_transition_date, :github, :linkedin, :personal_site, :other_info, :mentor])
+  end
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,56 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
-  def index
-    @users = policy_scope(User)
-    if params[:mentor] == true
-      @mentees = @users.where(mentor: false)
-    else
-      @mentors = @users.where(mentor: true)
-    end
-  end
+  # before_action :set_user, only: %i[show edit update destroy]
+  # def index
+  #   @users = policy_scope(User)
+  #   if params[:mentor] == true
+  #     @mentees = @users.where(mentor: false)
+  #   else
+  #     @mentors = @users.where(mentor: true)
+  #   end
+  # end
 
-  def show
-    @user = policy_scope(User).find(params[:id])
-  end
+  # def show
+  #   @user = policy_scope(User).find(params[:id])
+  # end
 
-  def edit
-    authorize @user
-  end
+  # def edit
+  #   authorize @user
+  # end
 
-  def update
-    authorize @user
-    @user.update(user_params)
-    redirect_to user_path(@user)
-  end
+  # def update
+  #   authorize @user
+  #   @user.update(user_params)
+  #   redirect_to user_path(@user)
+  # end
 
-  def new
-    @user = User.new
-    authorize @user
-  end
+  # def new
+  #   @user = User.new
+  #   authorize @user
+  # end
 
-  def create
-    @user = User.create(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new, status: :unprocessable_entity
-    end
-    authorize @user
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  #   authorize @user
+  # end
 
-  def destroy
-    authorize @user
-    @user.destroy
-    redirect_to root_path, notice: 'Usuário removido com sucesso.', status: :see_other
-  end
+  # def destroy
+  #   authorize @user
+  #   @user.destroy
+  #   redirect_to root_path, notice: 'Usuário removido com sucesso.', status: :see_other
+  # end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit!
-  end
+  # def user_params
+  #   params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :country, :state, :city, :professional_field, :academic_degree, :programming_language, :mentor_current_employer, :mentor_transition_date, :github, :linkedin, :personal_site, :other_info)
+  # end
 
-  def set_user
-    User.find(params[:id])
-  end
+  # def set_user
+  #   User.find(params[:id])
+  # end
 end
