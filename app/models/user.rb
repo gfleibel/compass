fe4d9341a964023cd96d7b262738c79913f0d8af
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2023-06-06 12:04:16 -0300

    cloudinary done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
