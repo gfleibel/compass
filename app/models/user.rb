2993da37042f79cbcd842b6590dbacf8a07fb20d
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2023-06-07 15:08:46 -0300

    still doing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :mentor_matches, class_name: 'Match', foreign_key: 'mentor_id'
-  has_many :mentee_matches, class_name: 'Match', foreign_key: 'mentee_id'
+  has_many :mentor_matches, class_name: 'Match', foreign_key: 'mentor_id', dependent: :destroy
+  has_many :mentee_matches, class_name: 'Match', foreign_key: 'mentee_id', dependent: :destroy
 
   validates :first_name, :last_name, :email, :country, :city, :state, :professional_field, :academic_degree, presence: true
 end
