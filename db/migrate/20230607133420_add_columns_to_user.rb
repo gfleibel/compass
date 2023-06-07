601f41b35c157fd92cd9829bfe0dc8386752af85
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-07 11:47:46 -0300

    migration

--- db/migrate/20230607133420_add_columns_to_user.rb
+++ db/migrate/20230607133420_add_columns_to_user.rb
@@ -3,5 +3,6 @@ class AddColumnsToUser < ActiveRecord::Migration[7.0]
     add_column :users, :about_me, :text
     add_column :users, :professional_about, :text
     add_column :users, :transition_about, :text
+
   end
 end
