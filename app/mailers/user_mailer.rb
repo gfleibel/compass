23794e64106ff95d8469f1307d2ea89f39c8306d
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-08 09:18:44 -0300

    emails mentor request

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,15 @@
 class UserMailer < ApplicationMailer
   def current_user
-    User.find(params[:user][:mentee_id])
+    User.find(params[:user])
   end
   def welcome_mentor
-    @mentor = User.find(:user) # Instance variable => available in view
-    mail(to: "vitoria.vital123@gmail.com", subject: 'Welcome to Le Wagon')
+    @mentee = User.find(params[:mentee])
+    @user = User.find(params[:user]) # Instance variable => available in view
+    mail(to: @user.email, subject: 'Compass | Solicitação de Mentoria')
     # This will render a view in `app/views/user_mailer`!
   end
   def welcome_mentee
+    @mentor = User.find(params[:mentor])
     @user = self.current_user
     mail(to: @user.email, subject: 'Compass | Confirmação de solicitação de mentoria')
     # This will render a view in `app/views/user_mailer`!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
