a24e96d9179c3d0e897f3d4bb4794ebcc4214134
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-08 16:59:08 -0300

    host mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://compass-gfleibel.herokuapp.com/" }
 end
