317ad7cecadacd25ffa001457e40c81c64bb6099
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-12 14:39:54 -0300

    migration

--- db/migrate/20230612165035_change_programming_language_to_array.rb
+++ db/migrate/20230612165035_change_programming_language_to_array.rb
@@ -1,5 +1,5 @@
 class ChangeProgrammingLanguageToArray < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :programming_language, :string, array: true, default: []
+    change_column :users, :programming_language, :string, array: true, default: [], using: "(string_to_array(programming_language, ','))"
   end
 end
