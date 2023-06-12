1bd8264cf185bf26bb9640e1379bf2c5d7bb4abb
Vitoria Vital <vitoria.vital123@gmail.com>
2023-06-12 17:48:11 -0300

    reset fixes

--- db/schema.rb
+++ db/schema.rb
@@ -69,17 +69,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_184435) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "programming_languages", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # create_table "programming_languages", force: :cascade do |t|
+  #   t.string "name"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
 
-  create_table "programming_languages_options", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # create_table "programming_languages_options", force: :cascade do |t|
+  #   t.string "name"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -115,12 +115,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_184435) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_programming_languages", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "programming_language_id", null: false
-    t.index ["programming_language_id", "user_id"], name: "index_programming_languages_users"
-    t.index ["user_id", "programming_language_id"], name: "index_users_programming_languages"
-  end
+  # create_table "users_programming_languages", id: false, force: :cascade do |t|
+  #   t.bigint "user_id", null: false
+  #   t.bigint "programming_language_id", null: false
+  #   t.index ["programming_language_id", "user_id"], name: "index_programming_languages_users"
+  #   t.index ["user_id", "programming_language_id"], name: "index_users_programming_languages"
+  # end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
