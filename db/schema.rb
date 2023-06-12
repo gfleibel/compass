e2b341de9e74bf7dc8e936a31893b6fddb7f569b
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-12 17:08:40 -0300

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -69,18 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_184435) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "programming_languages", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "programming_languages_options", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -113,13 +101,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_184435) do
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "users_programming_languages", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "programming_language_id", null: false
-    t.index ["programming_language_id", "user_id"], name: "index_programming_languages_users"
-    t.index ["user_id", "programming_language_id"], name: "index_users_programming_languages"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
