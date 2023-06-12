f45edff65c4cad9b65e1491f44a6868ba3da8846
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-12 09:10:50 -0300

    added recaptcha

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,24 @@ class PagesController < ApplicationController
     mail = ContactMailer.with(name: params[:name], email: params[:email], message: params[:message]).contact_email(params[:name], params[:email], params[:message])
     mail.deliver_now
     redirect_to root_path
+
+    # Verificar o reCAPTCHA
+    recaptcha_response = params['g-recaptcha-response']
+    recaptcha_secret_key = RECAPTCHA_PRIVATE_KEY
+
+    recaptcha_verification_url = "https://www.google.com/recaptcha/api/siteverify"
+    response = HTTParty.post(recaptcha_verification_url, body: { secret: recaptcha_secret_key, response: recaptcha_response })
+
+    if response['success']
+      # O reCAPTCHA foi validado com sucesso, continue com o envio do formulário
+      mail = ContactMailer.with(name: params[:name], email: params[:email], message: params[:message]).contact_email(params[:name], params[:email], params[:message])
+      mail.deliver_now
+      redirect_to root_path
+    else
+      # O reCAPTCHA falhou, lide com o erro
+      flash[:error] = "Erro na validação do reCAPTCHA"
+      redirect_to your_contact_form_path
+    end
   end
 
   def resources
