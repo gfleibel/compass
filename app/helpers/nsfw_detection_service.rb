5ddf17706b04f8cb93769ac4c00d2efd948b8612
Gabriel Leibel <gfleibel@hotmail.com>
2023-06-13 16:18:31 -0300

    nsfw

---
+++ app/helpers/nsfw_detection_service.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+require 'uri'
+
+class NsfwDetectionService
+  def initialize(image_url)
+    @image_url = image_url
+  end
+
+  Rails.application.config.api_key = 'c421317a4dmsh0290e6f7fab8b1ep189a92jsna807f15435cb'
+
+  def detect_nsfw_content
+    url = URI("https://nsfw-images-detection-and-classification.p.rapidapi.com/adult-content")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["content-type"] = 'application/json'
+    request["X-RapidAPI-Key"] = Rails.application.config.api_key
+    request["X-RapidAPI-Host"] = 'nsfw-images-detection-and-classification.p.rapidapi.com'
+    request.body = {
+      url: @image_url
+    }.to_json
+
+    response = http.request(request)
+    response.body
+
+    # response.body contém o JSON retornado
+    response_json = JSON.parse(response.body)
+
+    # Acessa o valor da chave "unsafe"
+    unsafe_value = response_json["unsafe"]
+    return unsafe_value
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validate :validate_nsfw_content
   has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
@@ -12,4 +11,18 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, :country, :city, :state, :professional_field, :academic_degree, presence: true
   validates :github, format: { with: /\A[A-Za-z0-9]+\z/ }, uniqueness: true, allow_blank: true
   validates :linkedin, format: { with: /https:\/\/www\.linkedin\.com\/in\/.*/ }, uniqueness: true, allow_blank: true
+
+  def validate_nsfw_content
+    return unless photo.attached? # Assumindo que você tem um campo de avatar anexado
+
+    # Obtenha a URL do avatar anexado
+    image_url = photo.url
+
+    # Execute a detecção de conteúdo NSFW
+    nsfw_service = NsfwDetectionService.new(image_url)
+    nsfw_result = nsfw_service.detect_nsfw_content
+
+    # Verifique se o resultado indica conteúdo NSFW
+    errors.add(:photo, 'contains NSFW content. Please upload a different image.') if nsfw_result == true
+  end
 end
